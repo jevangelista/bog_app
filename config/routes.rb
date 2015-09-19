114e8e49b985aec3d9dae8a76f2409bfb2db5e03
Jamie Eula Evangelista <jevangelista@Jamies-MacBook-Pro-2.local>
2015-09-18 23:46:11 -0700

    completed step 19. set up route, method, and form for edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  root to: 'creatures#index'
- 
-  get "/creatures" => 'creatures#index'
-  get '/creatures/new' => 'creatures#new'
-  # rake routes to check this route out
-  get '/creatures/:id' => 'creatures#show'
-  post '/creatures' => 'creatures#create'
+    root to: 'creatures#index'
 
+    # Add prefixes to routes using `as: "some_prefix"` syntax
+    get '/creatures', to: 'creatures#index', as: "creatures"
+
+    get '/creatures/new', to: 'creatures#new', as: "new_creature"
+
+    get '/creatures/:id', to: 'creatures#show', as: "creature"
+
+    get '/creatures/:id/edit', to: 'creatures#show', as: "edit_creature"        
+
+    post "/creatures", to: "creatures#create"
 end
