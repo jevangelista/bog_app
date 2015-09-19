3e34307dcea9c0bd03cfcd6aecd40703230fcc8d
Jamie Eula Evangelista <jevangelista@Jamies-MacBook-Pro-2.local>
2015-09-18 21:12:24 -0700

    completed step 9. created a new route for Creatures in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'creatures#index'
  
- get "/creatures" => "creatures#index"
+  get "/creatures" => 'creatures#index'
+  get '/creatures/new' => 'creatures#new'
 
 end
