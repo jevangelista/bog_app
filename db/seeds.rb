d5d5c62abd3a0d697dccc73646141e0b3691b272
Jamie Eula Evangelista <jevangelista@Jamies-MacBook-Pro-2.local>
2015-09-18 20:47:09 -0700

    completed step 6. created creatures index route in routes.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@ Creature.create([
 				{name: "Darth Vader", description: "Father of Luke"}
 				]) 
 
+# Creature.create({name: "Luke", description: "Jedi"}) 
+# Creature.create({name: "Darth Vader", description: "Father of Luke"}) 
